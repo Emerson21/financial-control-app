a61a76e33d96296ceea1c6ee0454a2f115fc8f22
Emerson Haraguchi <alonso.emerson21@gmail.com>
2021-10-17 16:56:23 -0300

    main - first commit creating maven spring boot project

---
+++ src/main/java/br/com/vr/development/emerson/chapter/financialcontrolapp/FinancialControlAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.vr.development.emerson.chapter.financialcontrolapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialControlAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialControlAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/vr/development/emerson/chapter/financialcontrolapp/FinancialControlAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vr.development.emerson.chapter.financialcontrolapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialControlAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
