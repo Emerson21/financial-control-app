42be505e6e54c145b7dcd761383b148722058cc3
Emerson Haraguchi <alonso.emerson21@gmail.com>
2021-11-18 22:34:11 -0300

    main - criado primeiro cenario de teste para abertura de conta corrente

--- src/test/java/br/com/vr/development/financialcontrolapp/inbound/resources/ContaResourceTest.java
+++ src/test/java/br/com/vr/development/financialcontrolapp/inbound/resources/ContaResourceTest.java
@@ -1,10 +1,15 @@
 package br.com.vr.development.financialcontrolapp.inbound.resources;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import br.com.vr.development.financialcontrolapp.application.inbound.ContaResource;
@@ -27,11 +32,16 @@ public class ContaResourceTest {
 
     @Test
     public void deveReceberDadosParaAberturaDaContaCorrente() {
-        
         String uri = "/v1/banco/conta";
 
+        FormularioAberturaConta formulario = new FormularioAberturaConta();
 
+        String jsonPayload = new ObjectMapper().writeValueAsString(formulario);
 
+        this.mockMvc.perform(MockMvcRequestBuilders.post(uri)
+            .content("")
+            .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(MockMvcResultMatchers.status().isCreated());
 
     }
 
