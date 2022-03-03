c192c2de370dad320ea04d0a73a988e0bb11211e
Emerson da Silva Alonso Haraguchi <emerson.haraguchi@vr.com.br>
2022-03-03 18:23:55 -0300

    main - refatorado TipoTransferencia

--- src/main/java/br/com/vr/development/financialcontrolapp/application/enums/TipoTransferencia.java
+++ src/main/java/br/com/vr/development/financialcontrolapp/application/enums/TipoTransferencia.java
@@ -4,28 +4,18 @@ import br.com.vr.development.financialcontrolapp.application.domain.model.Valor;
 
 public enum TipoTransferencia {
 
-    TED(new Valor("5")) {
+    TED(new Valor("5")),
+    TEF(Valor.ZERO);
 
-        @Override
-        public Valor aplicaTaxaNo(Valor valorTransferencia) {
-            return taxa.adicionar(valorTransferencia);
-        }
-    },
-    
-    TEF(Valor.ZERO) {
 
-        @Override
-        public Valor aplicaTaxaNo(Valor valorTransferencia) {
-            return taxa.adicionar(valorTransferencia);
-        }
-    };
-
-    Valor taxa;
+    private Valor taxa;
 
     TipoTransferencia(Valor taxa) {
         this.taxa = taxa;
     }
 
-    public abstract Valor aplicaTaxaNo(Valor valorTransferencia);
+    public Valor aplicaTaxaNo(Valor valorTransferencia) {
+        return taxa.adicionar(valorTransferencia);
+    }
 
 }
